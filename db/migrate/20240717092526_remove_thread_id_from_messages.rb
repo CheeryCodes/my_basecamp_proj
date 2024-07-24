ec2db935c5ea6bd5f8dc4d7ac70c3880c6bea06b
User <user@otni-gmb.ng>
2024-07-24 04:20:24 -0700

    Update a database migration

--- db/migrate/20240717092526_remove_thread_id_from_messages.rb
+++ db/migrate/20240717092526_remove_thread_id_from_messages.rb
@@ -1,5 +1,7 @@
 class RemoveThreadIdFromMessages < ActiveRecord::Migration[7.1]
   def change
-    remove_column :messages, :thread_id, :integer
+    if column_exists?(:messages, :thread_id)
+      remove_column :messages, :thread_id, :integer
+    end
   end
 end
