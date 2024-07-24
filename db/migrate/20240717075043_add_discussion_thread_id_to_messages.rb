96d4aaad0f11c7abd50d4465209e158b2b16671c
User <user@otni-gmb.ng>
2024-07-24 04:16:17 -0700

    Update a database migrations

--- db/migrate/20240717075043_add_discussion_thread_id_to_messages.rb
+++ db/migrate/20240717075043_add_discussion_thread_id_to_messages.rb
@@ -1,6 +1,8 @@
 class AddDiscussionThreadIdToMessages < ActiveRecord::Migration[7.1]
   def change
-    add_column :messages, :discussion_thread_id, :integer
-    add_index :messages, :discussion_thread_id
+    unless column_exists?(:messages, :discussion_thread_id)
+      add_column :messages, :discussion_thread_id, :integer
+      add_index :messages, :discussion_thread_id
+    end
   end
 end
